335cd6a8c098a97e958e3b0f8522b66754c876be
zzuzjq <984630262@qq.com>
2018-06-21 14:12:45 +0800

    1.0.1

--- RTMeetEngine/RTMeetEngine.framework/Headers/RTMeetKit.h
+++ RTMeetEngine/RTMeetEngine.framework/Headers/RTMeetKit.h
@@ -162,6 +162,23 @@
 
 - (BOOL)sendUserMessage:(NSString*)strUserName andUserHeader:(NSString*)strUserHeaderUrl andContent:(NSString*)strContent;
 
+#pragma mark - 视频流信息监测
+
+/**
+ 设置视频网络状态是否打开
+
+ @param bEnable YES:打开;NO:关闭
+ 说明:默认关闭
+ */
+- (void)setNetworkStatus:(BOOL)bEnable;
+
+/**
+ 获取当前视频网络状态是否打开
+
+ @return 获取网络视频状态
+ */
+- (BOOL)networkStatusEnabled;
+
 #pragma mark - 白板功能模块
 
 /**
